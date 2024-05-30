c43a7ca9d2a33b9d745a74ecfb9d2a876def0a55
omega1477 <34313991+omega1477@users.noreply.github.com>
2024-05-30 16:58:35 -0400

    Update variables.tf

--- modules/Landing_zone/variables.tf
+++ modules/Landing_zone/variables.tf
@@ -1,47 +1,47 @@
-variable "eonid" {
-  description = "Eon ID"
-  type        = string
-}
-
-variable "location" {
-  description = "Location"
-  type        = string
-}
-
 variable "lz_name" {
-  description = "Landing Zone Name"
+  description = "Name of the landing zone"
   type        = string
 }
 
 variable "short_name" {
-  description = "Short Name"
+  description = "Short name for the landing zone"
   type        = string
 }
 
 variable "short_env" {
-  description = "Short Environment"
+  description = "Short environment name"
   type        = string
 }
 
 variable "env_name" {
-  description = "Environment Name"
+  description = "Environment name"
   type        = string
 }
 
 variable "vnet_address_prefix" {
-  description = "VNet Address Prefix"
+  description = "Address prefix for the VNet"
+  type        = string
+}
+
+variable "eonid" {
+  description = "EON ID for tagging"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Name of the project"
   type        = string
 }
 
 variable "subnets" {
-  description = "Map of subnet configurations"
-  type        = map(object({
-    cidr                                  = string
-    nsg_alias                             = string
-    route_table_alias                     = string
-    service_endpoints                     = list(string)
-    enforce_private_link_service_network_policies = string
-    enforce_private_link_endpoint_network_policies = string
+  description = "Map of subnets"
+  type = map(object({
+    cidr                                   = string
+    nsg_alias                              = string
+    route_table_alias                      = string
+    service_endpoints                      = list(string)
+    enforce_private_link_service_network_policies = bool
+    enforce_private_link_endpoint_network_policies = bool
   }))
 }
 
@@ -50,7 +50,17 @@ variable "nsg_names" {
   type        = map(string)
 }
 
+variable "rt_names" {
+  description = "Map of route table names"
+  type        = map(string)
+}
+
 variable "swimlane_udr_names" {
   description = "Map of Swimlane UDR names"
   type        = map(string)
+}
+
+variable "location" {
+  description = "Azure region where resources will be deployed"
+  type        = string
 }
