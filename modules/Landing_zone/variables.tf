1a5f63c89ba1a1e965c45400613ae211eb1fa189
omega1477 <34313991+omega1477@users.noreply.github.com>
2024-05-30 16:40:52 -0400

    Update variables.tf

--- modules/Landing_zone/variables.tf
+++ modules/Landing_zone/variables.tf
@@ -1,52 +1,52 @@
+variable "eonid" {
+  description = "Eon ID"
+  type        = string
+}
+
+variable "location" {
+  description = "Location"
+  type        = string
+}
+
 variable "lz_name" {
-  description = "Name of the landing zone"
+  description = "Landing Zone Name"
   type        = string
 }
 
 variable "short_name" {
-  description = "Short name for the landing zone"
+  description = "Short Name"
   type        = string
 }
 
 variable "short_env" {
-  description = "Short environment name"
+  description = "Short Environment"
   type        = string
 }
 
 variable "env_name" {
-  description = "Environment name"
+  description = "Environment Name"
   type        = string
 }
 
 variable "vnet_address_prefix" {
-  description = "Address prefix for the VNet"
-  type        = string
-}
-
-variable "eonid" {
-  description = "EON ID for tagging"
+  description = "VNet Address Prefix"
   type        = string
 }
 
 variable "subnets" {
-  description = "Map of subnets"
-  type = map(object({
-    cidr                                   = string
-    nsg_alias                              = string
-    route_table_alias                      = string
-    service_endpoints                      = list(string)
-    enforce_private_link_service_network_policies = bool
-    enforce_private_link_endpoint_network_policies = bool
+  description = "Map of subnet configurations"
+  type        = map(object({
+    cidr                                  = string
+    nsg_alias                             = string
+    route_table_alias                     = string
+    service_endpoints                     = list(string)
+    enforce_private_link_service_network_policies = string
+    enforce_private_link_endpoint_network_policies = string
   }))
 }
 
 variable "nsg_names" {
   description = "Map of NSG names"
-  type        = map(string)
-}
-
-variable "rt_names" {
-  description = "Map of route table names"
   type        = map(string)
 }
 
