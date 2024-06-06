2ebc6ae116d43b63d37efb19f05d458db82870ef
nocode <110477540+auro-777@users.noreply.github.com>
2024-06-06 14:45:40 -0400

    Update main.tf

--- modules/rbac/main.tf
+++ modules/rbac/main.tf
@@ -1,5 +1,15 @@
-resource "azurerm_role_assignment" "assignment" {
-  scope                = var.scope
-  role_definition_name = var.role_definition_name
-  principal_id         = var.principal_id
+resource "azuread_group" "cloud_deployment" {
+  for_each = var.ad_groups
+  display_name     = each.value.group_name
+  security_enabled = true
+}
+
+resource "azurerm_role_assignment" "role_assignment" {
+  for_each = {
+    for group_key, group_value in var.ad_groups : group_key => group_value.role_names
+  }
+
+  principal_id       = azuread_group.cloud_deployment[each.key].object_id
+  role_definition_name = each.value
+  scope              = "/subscriptions/${var.subscription_id}"
 }
