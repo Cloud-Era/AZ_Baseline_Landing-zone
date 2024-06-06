cc6af14d626fdf10b1ea3559c3d313708f159b95
nocode <110477540+auro-777@users.noreply.github.com>
2024-06-06 14:45:16 -0400

    Update variables.tf

--- modules/rbac/variables.tf
+++ modules/rbac/variables.tf
@@ -12,3 +12,31 @@ variable "principal_id" {
   description = "ID of the principal to assign the role to."
   type        = string
 }
+###############
+variable "tenant_id" {
+  description = "The tenant ID for Azure"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "The subscription ID where resources will be deployed"
+  type        = string
+}
+
+variable "ad_groups" {
+  description = "Map of AD group names and their corresponding roles"
+  type = map(object({
+    group_name = string
+    role_names = list(string)
+  }))
+}
+
+variable "location" {
+  description = "The Azure region location"
+  type        = string
+}
+
+variable "env_name" {
+  description = "The environment name (e.g., dev, qa, prod)"
+  type        = string
+}
